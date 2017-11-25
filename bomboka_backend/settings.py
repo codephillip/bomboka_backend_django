9e398b3829d2cce8c9240f2509876b485b62ee04
codephillip <codephillip@gmail.com>
2017-11-25 17:29:36 +0300

    fixed collectstatic

--- bomboka_backend/settings.py
+++ bomboka_backend/settings.py
@@ -192,7 +192,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = 'staticfiles'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'busticket/static'),
+    os.path.join(BASE_DIR, 'bomboka/static'),
 )
 
 MEDIA_ROOT = "photos/"
